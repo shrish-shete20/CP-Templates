fc24aeb837794ee8986b124cb0e97a8d40d19855
Shrish Shete <107463412+shrish-shete20@users.noreply.github.com>
2025-05-22 23:20:54 +0530

    Update shrish-cptemp.cpp

--- Shrish Templates/shrish-cptemp.cpp
+++ Shrish Templates/shrish-cptemp.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
-#define MOD 1000000007
+#define MOD 998244353
 #define INF 1e18
 #define PI 3.141592653589793238462
 #define sz(x) ((int)(x).size())
@@ -10,12 +10,13 @@ using namespace std;
 #define mp make_pair
 #define ff first
 #define ss second
+#define int long long
 
 typedef long long ll;
 typedef unsigned long long ull;
 typedef long double lld;
 
-void __print(int x) {cerr << x;}
+// void __print(int x) {cerr << x;}
 void __print(long x) {cerr << x;}
 void __print(long long x) {cerr << x;}
 void __print(unsigned x) {cerr << x;}
@@ -42,6 +43,7 @@ void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v..
 #define debug(x...)
 #endif
 
+vector<int> dec_to_bi(ll num){vector<int>temp;while(num!=0){temp.push_back(num & 1);num>>=1;}vector<int> v2 (temp.rbegin(),temp.rend());temp.swap(v2);return temp;}//decimal to binary
 ll gcd(ll a, ll b) {if (b > a) {return gcd(b, a);} if (b == 0) {return a;} return gcd(b, a % b);}
 ll expo(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
 void extendgcd(ll a, ll b, ll*v) {if (b == 0) {v[0] = 1; v[1] = 0; v[2] = a; return ;} extendgcd(b, a % b, v); ll x = v[1]; v[1] = v[0] - v[1] * (a / b); v[0] = x; return;} //pass an arry of size1 3
@@ -56,10 +58,8 @@ ll mod_mul(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a * b) % m) + m) %
 ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
 ll mod_div(ll a, ll b, ll m) {a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m;}  //only for prime m
 ll phin(ll n) {ll number = n; if (n % 2 == 0) {number /= 2; while (n % 2 == 0) n /= 2;} for (ll i = 3; i <= sqrt(n); i += 2) {if (n % i == 0) {while (n % i == 0)n /= i; number = (number / i * (i - 1));}} if (n > 1)number = (number / n * (n - 1)) ; return number;} //O(sqrt(N))
-
-/*--------------------------------------------------------------------------------------------------------------------------*/
-
-int main()
+/*--*/
+signed main()
 {
     return 0;
 }
